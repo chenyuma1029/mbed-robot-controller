2221d9b042e075100d4579e4eecf9cddb93669a9
chenyuma1029 <2955282007@qq.com>
2026-04-10 15:38:13 +0800

    Add files via upload

---
+++ core/config.h
@@ -0,0 +1,49 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifdef __ARM_FP
+#undef __ARM_FP
+#endif
+
+#include "mbed.h"
+#include "MotionControl.h"
+#include "StdMotor.h"
+#include "Ultrasonic.hpp"
+
+#include <cstring>
+#include <cstdio>
+#include <cstdint>
+
+
+const int WIDTH  = 320;
+const int HEIGHT = 240;
+const int A      = WIDTH * HEIGHT;
+
+const int period     = 50;  // unit: us
+const int speedGrade = 7;
+
+
+const uint32_t BT_READABLE  = (1 << 0);
+const uint32_t RPI_READABLE = (1 << 1);
+
+
+struct CmdInfo {
+    char text[32];
+    int  len;
+    CmdInfo() { memset(text, 0, sizeof(text)); len = 0; }
+};
+
+
+extern StdMotor BR, BL, FR, FL;
+extern MotionControl carMotion;
+
+extern BufferedSerial pc;
+extern BufferedSerial bt;
+extern BufferedSerial rpi;
+extern Ultrasonic sonar;
+
+extern bool sonicSwitch;
+extern EventFlags flag;
+extern Mail<CmdInfo, 16> cmdMail;
+
+#endif 
